d82c0e7d9ae989a941b2da9ed8ed558e8a11775e
Jin114114 <90198894+Jin114114@users.noreply.github.com>
2021-11-23 22:16:16 +0800

    Update CodownBookTest by Ming Xia

---
+++ CodownBookTest/src/AccModifierTest.java
@@ -0,0 +1,19 @@
+public class AccModifierTest {
+
+    //test public
+    public  void testPublic(){
+
+    }
+
+    //test protected
+    protected   void testProtected(){
+
+    }
+
+    //test private
+    private  void testPrivate(){
+
+    }
+
+
+}
---
+++ CodownBookTest/src/ParamTest.java
@@ -0,0 +1,63 @@
+public class ParamTest {
+    //test none
+    public   void paramTest0( ){
+
+    }
+
+    //test byte
+    public   void paramTest1(byte a){
+
+    }
+
+    //test short
+    public void paramTest2(short a){
+
+    }
+
+    //test int
+    public  void paramTest3(int a){
+
+    }
+
+    //test long
+    public void paramTest4(long a){
+
+    }
+
+    //test float
+    public void paramTest5(float a){
+
+    }
+
+    //test double
+    public void paramTest6(double a){
+
+    }
+
+    //test boolean
+    public void paramTest7(boolean a){
+
+    }
+
+    //test char
+    public void paramTest8(char a){
+
+    }
+
+    //test String
+    public void paramTest9(String a){
+
+    }
+
+    //test StringBuilder
+    public void paramTest10(StringBuilder a){
+
+    }
+
+
+    //test developer-define data type
+    public void paramTest11(Calculator a){
+
+    }
+
+}
